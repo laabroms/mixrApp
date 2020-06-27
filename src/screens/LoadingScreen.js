f6e5e293d79fbbe20fdf6e61ab4b06824fd66087
Lucas Abroms <lucasabroms@Lucass-MacBook-Pro.local>
2020-06-27 11:49:28 -0700

    more files

---
+++ src/screens/LoadingScreen.js
@@ -0,0 +1,25 @@
+import React, { Component, useState } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+    StatusBar,
+    Button,
+    ActivityIndicator,
+    Image,
+} from 'react-native';
+
+
+export function LoadingScreen({navigation}) {
+    return (
+
+        <Text>Test</Text>
+
+
+    )
+
+
+
+}
